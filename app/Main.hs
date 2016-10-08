019ea78a0bf42f8f9c5c7c808ade21ede4669973
Karshan Sharma <karshan.sharma@gmail.com>
2016-10-07 17:36:10 -0700

    Get rid of state branching, no real performance gain so far

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ import           Data.Bits                 ((.|.))
 import qualified Data.ByteString           as BS (ByteString, append, drop,
                                                   length, take)
 import qualified Data.ByteString.Char8     as C (putStrLn)
-import qualified Data.ByteString.Lazy      as L (ByteString, foldrChunks,
+import qualified Data.ByteString.Lazy      as L (ByteString, drop, foldrChunks,
                                                  readFile)
 import qualified Data.ByteString.Unsafe    as BS (unsafeIndex)
 import           GHC.Base                  (Int (..), uncheckedShiftL#)
@@ -31,24 +31,18 @@ getWord32At :: Int -> BS.ByteString -> Word32
 {-# INLINE getWord32At #-}
 getWord32At n = word32le . BS.drop n
 
-data FoldState =
-    GetGlobalHeader
-  | GetPacket
-  | FailState String
-        deriving (Eq, Show)
-
 main :: IO ()
 main = do
     (fn:_) <- getArgs
     lbs <- L.readFile fn
-    evalStateT (parseAndPrintChunks lbs) (GetGlobalHeader, "")
+    evalStateT (parseAndPrintChunks (L.drop pcapGlobalHdrLen lbs)) ""
 
-parseAndPrintChunks :: L.ByteString -> StateT (FoldState, BS.ByteString) IO ()
+parseAndPrintChunks :: L.ByteString -> StateT BS.ByteString IO ()
 parseAndPrintChunks lbs =
     L.foldrChunks
         (\e a -> do
-            (state, leftover) <- get
-            put =<< liftIO (parseAndPrintChunk (leftover `BS.append` e) state)
+            leftover <- get
+            put =<< liftIO (parseAndPrintChunk (leftover `BS.append` e))
             a)
         (return ())
         lbs
@@ -64,35 +58,24 @@ pcapHdrMagic = 0xa1b2c3d4
 quotePktMagic :: BS.ByteString
 quotePktMagic = "B6034"
 
-parseAndPrintChunk :: BS.ByteString -> FoldState -> IO (FoldState, BS.ByteString)
-parseAndPrintChunk chunk state =
-    case state of
-        GetGlobalHeader ->
-            if BS.length chunk < pcapGlobalHdrLen then
-                return (GetGlobalHeader, chunk)
-            else
-                if getWord32At 0 chunk == pcapHdrMagic then
-                    parseAndPrintChunk (BS.drop pcapGlobalHdrLen chunk) GetPacket
-                else
-                    return (FailState "Not a pcap file", "")
-        GetPacket -> do
-            if BS.length chunk < pcapPktHdrLen then
-                return (GetPacket, chunk)
+parseAndPrintChunk :: BS.ByteString -> IO BS.ByteString
+parseAndPrintChunk chunk =
+    if BS.length chunk < pcapPktHdrLen then
+        return chunk
+    else do
+        let pktTime = (getWord32At 0 chunk, getWord32At 4 chunk)
+            pktLen = fromIntegral $ getWord32At 8 chunk
+            origLen = fromIntegral $ getWord32At 12 chunk
+            goNextPkt = parseAndPrintChunk (BS.drop (pcapPktHdrLen + pktLen) chunk)
+        if BS.length chunk < pcapPktHdrLen + pktLen then
+            return chunk
+        else
+            if origLen /= pktLen || pktLen < quotePktLen then
+                goNextPkt
             else do
-                let pktTime = (getWord32At 0 chunk, getWord32At 4 chunk)
-                    pktLen = fromIntegral $ getWord32At 8 chunk
-                    origLen = fromIntegral $ getWord32At 12 chunk
-                    goNextPkt = parseAndPrintChunk (BS.drop (pcapPktHdrLen + pktLen) chunk) GetPacket
-                if BS.length chunk < pcapPktHdrLen + pktLen then
-                    return (GetPacket, chunk)
-                else
-                    if origLen /= pktLen || pktLen < quotePktLen then
-                        goNextPkt
-                    else do
-                        let pktStart = BS.drop (pcapPktHdrLen + pktLen - quotePktLen) chunk
-                        if BS.take 5 pktStart /= quotePktMagic then do
-                            goNextPkt
-                        else do
-                            liftIO $ C.putStrLn (BS.take quotePktLen pktStart)
-                            goNextPkt
-        FailState msg -> return (FailState msg, "")
+                let pktStart = BS.drop (pcapPktHdrLen + pktLen - quotePktLen) chunk
+                if BS.take 5 pktStart /= quotePktMagic then do
+                    goNextPkt
+                else do
+                    liftIO $ C.putStrLn (BS.take quotePktLen pktStart)
+                    goNextPkt
