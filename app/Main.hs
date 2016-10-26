79d214c6ac8ef8926346c944eda3fe2c35becc34
Karshan Sharma <karshan.sharma@gmail.com>
2016-10-25 23:11:46 -0700

    hindent

--- app/Main.hs
+++ app/Main.hs
@@ -1,78 +1,79 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified Data.ByteString.Lazy          as L (ByteString, readFile)
-import qualified Data.ByteString.Lazy.Internal as L (ByteString (..))
-import           Data.Heap                     (Entry (..), Heap)
-import qualified Data.Heap                     as Heap
-import           Parser                        (runParser)
-import           QuoteParser                   (QuotePkt (..), pcapHdrParser,
-                                                printQuotePkt, quotePktParser)
-import           System.Environment            (getArgs)
-import           Time                          (Time (..), centiSecondsDiff)
+import qualified Data.ByteString.Lazy as L (ByteString, readFile)
+import qualified Data.ByteString.Lazy.Internal as L
+       (ByteString(..))
+import Data.Heap (Entry(..), Heap)
+import qualified Data.Heap as Heap
+import Parser (runParser)
+import QuoteParser
+       (QuotePkt(..), pcapHdrParser, printQuotePkt, quotePktParser)
+import System.Environment (getArgs)
+import Time (Time(..), centiSecondsDiff)
 
 usage :: IO ()
 usage = putStrLn "usage: ./pcap-stock-quote [-r] <pcap-file>"
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        ("-r":fn:[]) -> run True fn
-        (fn:"-r":[]) -> run True fn
-        (fn:[]) -> run False fn
-        _ -> usage
+  args <- getArgs
+  case args of
+    ("-r":fn:[]) -> run True fn
+    (fn:"-r":[]) -> run True fn
+    (fn:[]) -> run False fn
+    _ -> usage
 
 run :: Bool -> String -> IO ()
 run sort fn = do
-    lbs <- L.readFile fn
-    case runParser lbs pcapHdrParser of
-        Left s -> putStrLn $ "pcapHdrParser failed: " ++ s
-        Right (False, _) -> putStrLn "Error: not a pcap file"
-        Right (True, rest) ->
-            if sort then
-                runSort rest
-            else
-                runNormal rest
+  lbs <- L.readFile fn
+  case runParser lbs pcapHdrParser of
+    Left s -> putStrLn $ "pcapHdrParser failed: " ++ s
+    Right (False, _) -> putStrLn "Error: not a pcap file"
+    Right (True, rest) ->
+      if sort
+        then runSort rest
+        else runNormal rest
 
 type HeapEntry = Entry Time QuotePkt
 
 runSort :: L.ByteString -> IO ()
 runSort inp = go Heap.empty inp
-    where
-        go :: Heap HeapEntry -> L.ByteString -> IO ()
-        go h L.Empty = foldMap (printQuotePkt . payload) h
-        go h i =
-            case runParser i quotePktParser of
-                Right (a, rest) ->
-                    case a of
-                        (Left time) -> do
-                            h' <- flushHeap time h
-                            go h' rest
-                        (Right pkt) -> do
-                            h' <- flushHeap (pktTime pkt) h
-                            go (Heap.insert (Entry (acceptTime pkt) pkt) h') rest
-                Left s -> putStrLn $ "quotePktParser failed: " ++ s
+  where
+    go :: Heap HeapEntry -> L.ByteString -> IO ()
+    go h L.Empty = foldMap (printQuotePkt . payload) h
+    go h i =
+      case runParser i quotePktParser of
+        Right (a, rest) ->
+          case a of
+            (Left time) -> do
+              h' <- flushHeap time h
+              go h' rest
+            (Right pkt) -> do
+              h' <- flushHeap (pktTime pkt) h
+              go (Heap.insert (Entry (acceptTime pkt) pkt) h') rest
+        Left s -> putStrLn $ "quotePktParser failed: " ++ s
 
 -- Print all packets in the heap that have accept times more than 3 seconds in the past
 -- from the given time.
 flushHeap :: Time -> Heap HeapEntry -> IO (Heap HeapEntry)
 flushHeap t h =
-    case Heap.uncons h of
-        Just (minE, rest) ->
-            if t `centiSecondsDiff` priority minE > 300 then do
-                printQuotePkt (payload minE)
-                flushHeap t rest
-            else
-                return h
-        _ -> return h
+  case Heap.uncons h of
+    Just (minE, rest) ->
+      if t `centiSecondsDiff` priority minE > 300
+        then do
+          printQuotePkt (payload minE)
+          flushHeap t rest
+        else return h
+    _ -> return h
 
 runNormal :: L.ByteString -> IO ()
 runNormal L.Empty = return ()
 runNormal i =
-    case runParser i quotePktParser of
-        Right (a, rest) ->
-            case a of
-                (Left _) -> runNormal rest
-                (Right pkt) -> printQuotePkt pkt >> runNormal rest
-        Left s -> putStrLn $ "quotePktParser failed: " ++ s
+  case runParser i quotePktParser of
+    Right (a, rest) ->
+      case a of
+        (Left _) -> runNormal rest
+        (Right pkt) -> printQuotePkt pkt >> runNormal rest
+    Left s -> putStrLn $ "quotePktParser failed: " ++ s
--- app/QuoteParser.hs
+++ app/QuoteParser.hs
@@ -1,58 +1,63 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module QuoteParser where
 
-import qualified Data.ByteString       as BS (ByteString, concat, drop, head,
-                                              intercalate, last, take)
+import qualified Data.ByteString as BS
+       (ByteString, concat, drop, head, intercalate, last, take)
 import qualified Data.ByteString.Char8 as C (pack, putStrLn)
-import           Data.Monoid           ((<>))
-import           GHC.Word              (Word32 (..))
-import           Parser                (Parser, bytes, skip, word32)
-import           Time                  (Time (..), pcapTimeToTime)
-import           Util                  (dropTake)
+import Data.Monoid ((<>))
+import GHC.Word (Word32(..))
+import Parser (Parser, bytes, skip, word32)
+import Time (Time(..), pcapTimeToTime)
+import Util (dropTake)
 
 type QtyPrice = (BS.ByteString, BS.ByteString)
 
-data QuotePkt = QuotePkt {
-    pktTime    :: !Time
+data QuotePkt = QuotePkt
+  { pktTime :: !Time
   , acceptTime :: !Time
-  , issueCode  :: !BS.ByteString
-  , bids       :: [QtyPrice] -- TODO Vec ?
-  , asks       :: [QtyPrice]
-} deriving (Eq, Show)
+  , issueCode :: !BS.ByteString
+  , bids :: [QtyPrice] -- TODO Vec ?
+  , asks :: [QtyPrice]
+  } deriving (Eq, Show)
 
 parseAcceptTime :: BS.ByteString -> Time
 parseAcceptTime inp =
-    Time {
-        t_hours = readInt $ BS.take 2 inp
-      , t_minutes = readInt $ dropTake 2 2 inp
-      , t_seconds = readInt $ dropTake 4 2 inp
-      , t_centiseconds = readInt $ dropTake 6 2 inp
-    }
-    where
-        readInt b = ((fromIntegral (BS.head b) - 0x30) * 10) +
-                        (fromIntegral (BS.last b) - 0x30)
+  Time
+  { t_hours = readInt $ BS.take 2 inp
+  , t_minutes = readInt $ dropTake 2 2 inp
+  , t_seconds = readInt $ dropTake 4 2 inp
+  , t_centiseconds = readInt $ dropTake 6 2 inp
+  }
+  where
+    readInt b =
+      ((fromIntegral (BS.head b) - 0x30) * 10) +
+      (fromIntegral (BS.last b) - 0x30)
 
 -- The returned list is in reverse order w.r.t. the input stream
 parseNQtyPrice :: Int -> BS.ByteString -> [QtyPrice]
 parseNQtyPrice = go []
-    where
-        go acc 0 _ = acc
-        go acc n inp = go ((dropTake 5 7 inp, BS.take 5 inp):acc) (n - 1) (BS.drop 12 inp)
+  where
+    go acc 0 _ = acc
+    go acc n inp =
+      go ((dropTake 5 7 inp, BS.take 5 inp) : acc) (n - 1) (BS.drop 12 inp)
 
 parseQuotePkt :: Time -> BS.ByteString -> QuotePkt
 parseQuotePkt inPktTime rawPkt =
-    QuotePkt {
-        pktTime = inPktTime
-      , acceptTime = parseAcceptTime $ dropTake 206 8 rawPkt
-      , issueCode = dropTake 5 12 rawPkt
-      , bids = parseNQtyPrice 5 $ BS.drop 29 rawPkt
-      , asks = parseNQtyPrice 5 $ BS.drop 96 rawPkt
-    }
+  QuotePkt
+  { pktTime = inPktTime
+  , acceptTime = parseAcceptTime $ dropTake 206 8 rawPkt
+  , issueCode = dropTake 5 12 rawPkt
+  , bids = parseNQtyPrice 5 $ BS.drop 29 rawPkt
+  , asks = parseNQtyPrice 5 $ BS.drop 96 rawPkt
+  }
 
 pcapGlobalHdrLen, quotePktLen, pcapPktHdrLen :: Int
 pcapGlobalHdrLen = 24
+
 pcapPktHdrLen = 16
+
 quotePktLen = 215
 
 pcapHdrMagic :: Word32
@@ -63,42 +68,48 @@ quotePktMagic = "B6034"
 
 pcapHdrParser :: Parser Bool
 pcapHdrParser = do
-    magic <- word32
-    skip (pcapGlobalHdrLen - 4)
-    return $ magic == pcapHdrMagic
+  magic <- word32
+  skip (pcapGlobalHdrLen - 4)
+  return $ magic == pcapHdrMagic
 
 quotePktParser :: Parser (Either Time QuotePkt)
 quotePktParser = do
-    pTime <- (curry pcapTimeToTime) <$> word32 <*> word32
-    pktLen <- fromIntegral <$> word32
-    origLen <- fromIntegral <$> word32
-    if origLen /= pktLen || pktLen < quotePktLen then do
-        skip pktLen
-        return (Left pTime)
+  pTime <- (curry pcapTimeToTime) <$> word32 <*> word32
+  pktLen <- fromIntegral <$> word32
+  origLen <- fromIntegral <$> word32
+  if origLen /= pktLen || pktLen < quotePktLen
+    then do
+      skip pktLen
+      return (Left pTime)
     else do
-        skip (pktLen - quotePktLen)
-        magic <- bytes 5
-        if magic /= quotePktMagic then do
-            skip (quotePktLen - 5)
-            return (Left pTime)
-        else
-            (Right . parseQuotePkt pTime . (quotePktMagic <>)) <$> bytes (quotePktLen - 5)
+      skip (pktLen - quotePktLen)
+      magic <- bytes 5
+      if magic /= quotePktMagic
+        then do
+          skip (quotePktLen - 5)
+          return (Left pTime)
+        else (Right . parseQuotePkt pTime . (quotePktMagic <>)) <$>
+             bytes (quotePktLen - 5)
 
 printQuotePkt :: QuotePkt -> IO ()
-printQuotePkt QuotePkt{..} =
-    C.putStrLn $ timeS pktTime <> " " <> timeS acceptTime <> " " <> issueCode <> " "
-        <> BS.concat (map (\(q, p) -> q <> "@" <> p <> " ") bids)
-        <> BS.intercalate " " (map qtyPriceStr $ reverse asks)
-    where
-       qtyPriceStr (q, p) = q <> "@" <> p
+printQuotePkt QuotePkt {..} =
+  C.putStrLn $
+  timeS pktTime <> " " <> timeS acceptTime <> " " <> issueCode <> " " <>
+  BS.concat (map (\(q, p) -> q <> "@" <> p <> " ") bids) <>
+  BS.intercalate " " (map qtyPriceStr $ reverse asks)
+  where
+    qtyPriceStr (q, p) = q <> "@" <> p
         {- removeLeadingZeros, unused
         rlz a = let nlz = BS.length (BS.takeWhile (== 0x30) a)
                 in if nlz == BS.length a then "0" else BS.drop nlz a -}
 
 timeS :: Time -> BS.ByteString
-timeS t = C.pack $ padShow (t_hours t)
-            ++ padShow (t_minutes t)
-            ++ padShow (t_seconds t)
-            ++ padShow (t_centiseconds t)
-    where
-        padShow x = if x < 10 then '0':show x else show x
+timeS t =
+  C.pack $
+  padShow (t_hours t) ++
+  padShow (t_minutes t) ++ padShow (t_seconds t) ++ padShow (t_centiseconds t)
+  where
+    padShow x =
+      if x < 10
+        then '0' : show x
+        else show x
